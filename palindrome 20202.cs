277749ba7a71346c5d1dc2827b2288b10a3803d2
reubencleave <61647050+reubencleave@users.noreply.github.com>
2020-03-30 16:41:11 +1300

    Add files via upload

---
+++ palindrome 20202.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome;
+
+namespace palindrome_unit_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPalindromeTrue()
+        {
+            var test = new Form1();
+            String Word = "aassaa";
+            string answer = test.CheckPalindrome(Word);
+            Assert.AreEqual(Word + "is a palindrome", answer);
+        }
+        [TestMethod]
+    }
+    public void TestPalindromeFalse()
+    {
+        var test = new Form1();
+        string Word = "aass";
+        string answer = test.CheckPalindrome(Word);
+        Assert.AreEqual(Word + "is not a palindrome");
+    }
+}
